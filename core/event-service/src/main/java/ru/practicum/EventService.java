17732a20c4e8796317db09002c6e9640e2658943
Akim Kolesnik <kolakim@yandex.ru>
2026-07-15 12:16:13 +0300

    добавил ComponentScan

--- core/event-service/src/main/java/ru/practicum/EventService.java
+++ core/event-service/src/main/java/ru/practicum/EventService.java
@@ -2,10 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import ru.practicum.exception.ErrorHandler;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum"})
 @Import({ErrorHandler.class})
 public class EventService {
     public static void main(String[] args) {
--- core/request-service/src/main/java/ru/practicum/RequestService.java
+++ core/request-service/src/main/java/ru/practicum/RequestService.java
@@ -2,11 +2,13 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import ru.practicum.exception.ErrorHandler;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum"})
 @Import({ErrorHandler.class})
 public class RequestService {
     public static void main(String[] args) {
